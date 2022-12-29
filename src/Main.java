8718d1931ea6399c46e3f0d321b7795bcaf51bb9
dmcgalliard <88689991+dmcgalliard@users.noreply.github.com>
2022-12-29 13:45:58 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //creates instance of Person
         Person player = new Person();
-
+        //prompts user to enter needed values
         player.setName(player);
         player.setAge(player);
         player.setOccupation(player);
-
+        
+        //displays values
         System.out.println("Name: " + player.getName());
         System.out.println("Age: " + player.getAge());
         System.out.println("Occupation: " + player.getOccupation());
